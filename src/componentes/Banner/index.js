1f2f2c62283971dc0e00e2afecf47e78388dfdbb
Afranio Nunes Dantas <afranio.dantas@livsaude.com.br>
2025-04-04 14:24:11 -0300

    first commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="imagem do banner principal"/>
+    </header>
+  )
+}
+
+export default Banner
